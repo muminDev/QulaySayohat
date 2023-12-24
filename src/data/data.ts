dc4c51b7b4aeca1bbdb23d4bacb26fe863c1aba8
Muminjon <juramirzaev.muminjon@gmail.com>
2023-12-24 14:14:10 +0100

    main page generated

---
+++ src/data/data.ts
@@ -0,0 +1,34 @@
+const dataMain = [
+  {
+    id: 1,
+    name: 'Polshadagi hizmatlar va ularning manzillari',
+    img: 'warsaw'
+  },
+  {
+    id: 2,
+    name: 'Polshadagi hizmatlar va ularning manzillari',
+    img: 'warsaw'
+  },
+  {
+    id: 3,
+    name: 'Polshadagi hizmatlar va ularning manzillari',
+    img: 'warsaw'
+  },
+  {
+    id: 4,
+    name: 'Polshadagi hizmatlar va ularning manzillari',
+    img: 'warsaw'
+  },
+  {
+    id: 5,
+    name: 'Polshadagi hizmatlar va ularning manzillari',
+    img: 'warsaw'
+  },
+  {
+    id: 6,
+    name: 'Polshadagi hizmatlar va ularning manzillari',
+    img: 'warsaw'
+  }
+];
+
+export default dataMain;
